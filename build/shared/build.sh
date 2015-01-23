fd0006c8e5cfb2c07f1db0508fd7454eff3be5d2
Thomas Orozco <thomas@orozco.fr>
2015-01-23 18:14:11 +0100

    Set git identity

--- build/shared/build.sh
+++ build/shared/build.sh
@@ -3,9 +3,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+git config --global user.email "builder@scalr.com"
+git config --global user.name "Scalr Builder"
+
 # Prepare the build
 cd /builder
- 
+
 # Launch build
 echo "Building: ${SCALR_VERSION}"
 bin/omnibus build scalr-server
